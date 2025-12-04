e10fcf1c22102fce6c1adad3b940d13c392b9b36
Vlad Kats <mars77@gmail.com>
2025-12-04 11:42:01 -0700

    completed 11.6 - fixed eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+const mapResults = ({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1]),
+  }))
 
 const App = () => {
   const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const {
+    data: pokemonList,
+    error,
+    isLoading,
+  } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
   if (isLoading) {
     return <LoadingSpinner />
@@ -35,9 +40,11 @@ const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
+      <Route
+        exact
+        path="/pokemon/:name"
+        element={<PokemonPage pokemonList={pokemonList} previous={previous} next={next} />}
+      />
     </Routes>
   )
 }
