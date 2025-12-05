46e2d0a7787c1a48757d74868c0c11a39197f1cb
Vlad Kats <mars77@gmail.com>
2025-12-05 08:18:02 -0700

    added test to 11.9

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -8,4 +8,13 @@ describe('Pokedex', () => {
       page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')
     ).toBeVisible()
   })
+
+  test('a single pokemon can be viewed', async ({ page }) => {
+    await page.goto('/pokemon/raichu')
+    await expect(page.getByText('raichu')).toBeVisible()
+
+    const spAttack = await page.getByText('special attack')
+    expect(spAttack).toBeVisible()
+    await expect(page.getByText('lightning rod')).toBeVisible()
+  })
 })
